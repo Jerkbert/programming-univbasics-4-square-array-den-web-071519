6c37c3024e3195e2e2597fc065e4788c7f840e00
Rob Ballantyne <raballantyne@gmail.com>
2019-06-28 19:46:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   squared = []
   counter = 0
     while array[counter] do
-    squared << array[counter] ** 2
-    counter += 1
+      squared << array[counter] ** 2
+      counter += 1
     end
   squared
 end
